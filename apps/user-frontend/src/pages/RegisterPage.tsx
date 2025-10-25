bf70822630a1acc6ec1308947d63ab6d8ea32a23
darkmage208 <harukaleonhart@gmail.com>
2025-10-25 12:32:03 -0700

    feat: disable google login

--- apps/user-frontend/src/pages/RegisterPage.tsx
+++ apps/user-frontend/src/pages/RegisterPage.tsx
@@ -126,7 +126,7 @@ const RegisterPage = () => {
               </div>
             </div>
             
-            <Button
+            {/* <Button
               type="button"
               variant="outline"
               className="w-full mt-4"
@@ -140,7 +140,7 @@ const RegisterPage = () => {
                 <path fill="currentColor" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
               </svg>
               Continuar com Google
-            </Button>
+            </Button> */}
           </div>
         </CardContent>
         <CardFooter className="justify-center">
